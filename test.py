97739b5220d2bcb8526a3fbc125a2abcb3c794f1
vchuardi <44575986+vchuardi@users.noreply.github.com>
2019-04-19 17:59:47 +0700

    Update test.py

--- test.py
+++ test.py
@@ -5,36 +5,8 @@ import sys
 
 class m:
     def getAnswer(pertanyaan,question,answer,ignore):
-        # File eksternal Question dan Answer terpisah
-        # if len(sys.argv) > 1:
-        #     # Read Question
-        #     fin = open(sys.argv[1])
-        #     for line in fin:
-        #         line = line.strip('\n')
-        #         question.append(line)
-        #     fin.close
-
-        #     # Read Answer
-        #     fin = open(sys.argv[2])
-        #     for line in fin:
-        #         line = line.strip('\n')
-        #         answer.append(line)
-        #     fin.close
-
-        # print(question)
         questionsize = len(question)
-        # print(questionsize)
-        # print()
-        # print("Hi, selamat datang di chatbot Xyli")
-        # print("Apakah anda memiliki pertanyaan yang ingin ditanyakan?")
-        # print("Anda : ", end='')
-        # pertanyaan = input()
-        # pertanyaan = pertanyaan.split(" ")
-
-        # while pertanyaan[0]!="exit" :
         isAnswerExist = True
-
-        # print("Xyli : ", end='')
 
         for idx,sentence in enumerate(question):
             for kata in pertanyaan:
@@ -74,4 +46,5 @@ for i in range(1,qlen+1):
         pertanyaan = sys.argv[i]
         continue
     pertanyaan = pertanyaan + " " + sys.argv[i]
+print(pertanyaan)
 print(m1.getAnswer(pertanyaan,question,answer,ignore))
